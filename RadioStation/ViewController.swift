f951f88859b2294e10ea8b3f7118f0f3c2a88393
Abass Sesay <bassco10@gmail.com>
2015-11-12 19:53:12 -0500

    add 2 different stations accessed by 2 different buttons

--- RadioStation/ViewController.swift
+++ RadioStation/ViewController.swift
@@ -11,15 +11,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var myStation: RadioStation
+    var myStation2: RadioStation
     
     @IBOutlet var stationName : UILabel!
     @IBOutlet var stationFreq : UILabel!
     @IBOutlet var stationBand : UILabel!
     
     required init?(coder aDecoder: NSCoder) {
+        myStation2 = RadioStation()
+        myStation2.freq = 125.5
+        myStation2.name = "Knix"
+        
         myStation = RadioStation()
-        myStation.freq = 125.5
-        myStation.name = "Knix"
+        myStation.freq = 60
+        myStation.name = "Kn0x"
         super.init(coder: aDecoder)
     }
     
@@ -35,14 +40,14 @@ class ViewController: UIViewController {
 
     @IBAction func buttonClick(sender: AnyObject) {
         stationName.text = myStation.name
-        stationFreq.text = String(format: "%.1f",myStation.freq)
-        
-        if myStation.freq >= RadioStation.maxFMFrequency() && myStation.freq <= RadioStation.maxFMFrequency(){
-            stationBand.text = "FM"
-        }else{
-            stationBand.text = "AM"
-        }
+        stationFreq.text = String(format: "%.1f", myStation.freq)
+        stationBand.text = "AM"
     }
 
+    @IBAction func button2Click(sender: AnyObject) {
+        stationName.text = myStation2.name
+        stationFreq.text = String(format: "%.1f", myStation2.freq)
+        stationBand.text = "FM"
+    }
 }
 
